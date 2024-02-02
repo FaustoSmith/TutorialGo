6d34b5988247b71f5d44502ba8f20226d6026126
faustosmith <faustobfa@gmail.com>
2024-02-02 20:57:48 +0100

    Printf

--- main.go
+++ main.go
@@ -36,6 +36,8 @@ func main() {
 	var scoreTwo float64 = 65.6
 	scoreThree := 1.5
 
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
 	//cons
 	const secondsInMinute = 60
 	const minutesInHour = 60
@@ -44,12 +46,17 @@ func main() {
 	//Printf
 	age := 30
 	name := "Fausto"
+	salary := 1200.5378
 
 	fmt.Print("Hello, ")
 	fmt.Print("world! \n")
 	fmt.Print("!!\n")
-	fmt.Print("My age is", age, "and my name is", name)
+	fmt.Print("My age is", age, "and my name is", name, "\n")
 
-	fmt.Print("My age is %v and my name is %v", age, name)
+	fmt.Printf("My age is %v and my name is %v", age, name)                              //%v es el generico,toma el valor por defecto de la variable
+	fmt.Printf("My age is %v and my name is %s", age, name)                              //%s es para interpolar un string
+	fmt.Printf("My age is %d and my name is %v", age, name)                              //%d interpolar un entero en lugar de que salga como binario
+	fmt.Printf("My age is %v , my name is %v and  my salary is %f", age, name, salary)   //f interpolar un float
+	fmt.Printf("My age is %v , my name is %v and  my salary is %.2f", age, name, salary) //f interpolar un float, el .2 lo redondea
 
 }
