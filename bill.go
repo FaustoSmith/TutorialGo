24f8d75bd6afbf7e54ccc9d3339d60f7b17d5ca5
faustosmith <faustobfa@gmail.com>
2024-02-03 19:12:16 +0100

    Map,Function,Struct

---
+++ bill.go
@@ -0,0 +1,22 @@
+package main
+
+type bill struct { //definimos el nombre de la estructura y sus parametros
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func createBill(name string) bill { //constructor de la estructura
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+	return b
+}
+
+func (b bill) format() string {
+
+	return b.name
+}
